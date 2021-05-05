35dbb0d723aaa7712604e55441b00a04fea4189d
Castro-D <castro.dh91@gmail.com>
2021-05-05 16:39:42 -0300

    adds pokemon names to the page

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+const $frames = document.querySelectorAll('.frame');
+const POKEMONSPERPAGE = 20;
+
 fetch('https://pokeapi.co/api/v2/pokemon/')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        for (let i=0; i<20; i++) {
+            let content = document.createTextNode(`${data['results'][i].name}`);
+            $frames[i].appendChild(content);
+        };
+    });
 
