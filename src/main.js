2bcd64ede146533cabe0181cbc5293abfcdc5086
Castro-D <castro.dh91@gmail.com>
2021-05-17 15:43:08 -0300

    finish formatting code

--- src/main.js
+++ src/main.js
@@ -15,118 +15,117 @@ function getListOfApiUrl() {
   return listOfApiUrl;
 }
 
+function resetTextNodes() {
+  $frames.forEach(($frame) => {
+    $frame.textContent = '';
+  });
+}
+
+function fetchApiUrls(url, page) {
+  return fetch(`${url[page]}`).then((response) => response.json());
+}
+
 function displayPokemons(page) {
-    let listOfApiUrl = getListOfApiUrl();
-    resetTextNodes()
-    page--;
+  const listOfApiUrl = getListOfApiUrl();
+  resetTextNodes();
+  page -= 1;
 
-    const apiResponse = fetchApiUrls(listOfApiUrl, page);
-        apiResponse.then((data) => {
-            for (let i=0; i<POKEMONSPERPAGE; i++) {
-                let content = document.createTextNode(`${data['results'][i].name}`);
-                $frames[i].appendChild(content);
-            };
-        })
+  const apiResponse = fetchApiUrls(listOfApiUrl, page);
+  apiResponse.then((data) => {
+    for (let i = 0; i < POKEMONSPERPAGE; i += 1) {
+      const content = document.createTextNode(`${data.results[i].name}`);
+      $frames[i].appendChild(content);
+    }
+  });
 }
 
-function fetchApiUrls (url, page) {
-    return fetch(`${url[page]}`).then(response => response.json());
+function paginationButton(page) {
+  const button = document.createElement('button');
+  button.innerText = page;
+  if (CURRENTPAGE === page) button.classList.add('active');
+  button.addEventListener('click', () => {
+    CURRENTPAGE = page;
+    displayPokemons(CURRENTPAGE);
+    const currentBtn = document.querySelector('.pagenumbers button.active');
+    currentBtn.classList.remove('active');
+    button.classList.add('active');
+  });
+
+  return button;
 }
 
-function setUpPagination(pokemons, pokemonsperpage, wrapper){
-    let pageCount = Math.ceil(pokemons / pokemonsperpage)
-    for (let i = 1; i < pageCount + 1; i++) {
-		let btn = paginationButton(i);
-		wrapper.appendChild(btn);
-	}
+function setUpPagination(pokemons, pokemonsperpage, wrapper) {
+  const pageCount = Math.ceil(pokemons / pokemonsperpage);
+  for (let i = 1; i < pageCount + 1; i += 1) {
+    const btn = paginationButton(i);
+    wrapper.appendChild(btn);
+  }
 }
 
-function paginationButton(page){
-    let button = document.createElement('button');
-    button.innerText = page;
-    if (CURRENTPAGE == page) button.classList.add('active');
-    button.addEventListener('click', function(){
-        CURRENTPAGE = page;
-        displayPokemons(CURRENTPAGE);
-        let current_btn = document.querySelector('.pagenumbers button.active');
-        current_btn.classList.remove('active');
-        button.classList.add('active');     
-    })
-    
-    return button
+function closeModal() {
+  const span = document.querySelector('.close');
+
+  span.onclick = () => {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  };
 }
 
-function resetTextNodes(){
-    $frames.forEach(function ($frame){
-        $frame.textContent = '';
-        return;
-    })
+function showModal() {
+  modal.style.display = 'block';
 }
 
-function closeModal () {
-    let span = document.querySelector('.close');
+function resetPokemonInfo(pokemonImage, abilitiesText, pokemonHeight, pokemonType, pokemonWeight) {
+  pokemonImage.src = '';
+  abilitiesText.textContent = '';
+  pokemonHeight.textContent = '';
+  pokemonType.textContent = '';
+  pokemonWeight.textContent = '';
+}
 
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
-    
-    window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        };
-    };
+function displayPokemonHeader(modalHeader, pokemon) {
+  modalHeader.textContent = pokemon;
+}
+
+function getPokemonData(pokemon) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => response.json());
 }
 
 function showPokemonsInfo() {
-    let modalHeader = document.querySelector('.modal-header');
-    let pokemonImage = document.querySelector('#pokemon-image');
-    const pokemonHeight = document.querySelector('#pokemon-height');
-    const pokemonType = document.querySelector('#pokemon-type');
-    const pokemonWeight = document.querySelector('#pokemon-weight');
-    let abilitiesText = document.querySelector('#abilities-text');
+  const modalHeader = document.querySelector('.modal-header');
+  const pokemonImage = document.querySelector('#pokemon-image');
+  const pokemonHeight = document.querySelector('#pokemon-height');
+  const pokemonType = document.querySelector('#pokemon-type');
+  const pokemonWeight = document.querySelector('#pokemon-weight');
+  const abilitiesText = document.querySelector('#abilities-text');
 
-    $frames.forEach(function($frame){
-        $frame.addEventListener('click', (e) => { 
-            const pokemon = e.target.innerHTML;
+  $frames.forEach(($frame) => {
+    $frame.addEventListener('click', (e) => {
+      const pokemon = e.target.innerHTML;
 
-            showModal(); 
-            resetPokemonInfo(pokemonImage, abilitiesText, pokemonHeight, pokemonType, pokemonWeight);  
-            displayPokemonHeader(modalHeader, pokemon);
+      showModal();
+      resetPokemonInfo(pokemonImage, abilitiesText, pokemonHeight, pokemonType, pokemonWeight);
+      displayPokemonHeader(modalHeader, pokemon);
 
-            pokemonsResponse = getPokemonData(pokemon)
-                .then(data => {
-                    pokemonImage.src = data.sprites['front_default'];
-                    pokemonHeight.innerHTML = `Height: ${data.height}`;
-                    pokemonType.innerHTML = `Type: ${data.types['0']['type'].name}`;
-                    pokemonWeight.innerHTML = `Weight: ${data.weight}`;
-    
-                    data['abilities'].forEach(function (ability){
-                        abilitiesText.textContent += `${ability['ability'].name}, `;
-                    });      
-                });           
+      getPokemonData(pokemon)
+        .then((data) => {
+          pokemonImage.src = data.sprites.front_default;
+          pokemonHeight.innerHTML = `Height: ${data.height}`;
+          pokemonType.innerHTML = `Type: ${data.types['0'].type.name}`;
+          pokemonWeight.innerHTML = `Weight: ${data.weight}`;
+
+          data.abilities.forEach((ability) => {
+            abilitiesText.textContent += `${ability.ability.name}, `;
+          });
         });
     });
-}
-
-function showModal () {
-    modal.style.display = "block";
-}
-
-function getPokemonData (pokemon) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        .then(response => response.json())
-}
-
-function resetPokemonInfo (pokemonImage, abilitiesText, pokemonHeight, pokemonType, pokemonWeight) {
-    pokemonImage.src = '';
-    abilitiesText.textContent = ``;
-    pokemonHeight.textContent = '';
-    pokemonType.textContent = '';
-    pokemonWeight.textContent = '';
-}
-
-function displayPokemonHeader (modalHeader, pokemon) {
-    modalHeader.textContent = pokemon;
+  });
 }
 
 closeModal();
