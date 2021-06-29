668566d6f552c9a0a54a7f2027443eeb2390f0a4
Castro-D <castro.dh91@gmail.com>
2021-06-29 08:49:43 -0300

    create pokemon class

---
+++ src/classes.js
@@ -0,0 +1,10 @@
+export default class Pokemon {
+  constructor(name, image, height, type, weight, abilities) {
+    this.name = name;
+    this.image = image;
+    this.height = height;
+    this.type = type;
+    this.weight = weight;
+    this.abilities = abilities;
+  }
+}
